6d11a5c89101511b76f93413081e4b87b6e00820
greengen <green_generation@yahoo.com>
2011-07-04 08:32:26 +0500

    added authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,10 @@ class User < ActiveRecord::Base
     return nil  if user.nil?
     return user if user.has_password?(submitted_password)
   end
+  
 
+  
+  
   private
 
     def encrypt_password
